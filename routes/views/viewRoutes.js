7277a0e624c1e58a6e89c761f49aa731d834b2fd
Marie Nabors <marienabors@thefirstacademy.org>
2018-03-31 07:09:09 -0400

    updated saved route

--- routes/views/viewRoutes.js
+++ routes/views/viewRoutes.js
@@ -1,7 +1,12 @@
 //create multiple routes, starting with home route
 module.exports = function (app){
 	app.get("/", function(req, res){
-		res.render("./views/layouts/main.html");
+		res.render("./views/layouts/main.handlebars");
     });
 }
 //add saved route
+module.exports = function (app){
+	app.get("/", function(req, res){
+		res.render("./views/savedPage.handlebars");
+    });
+}
