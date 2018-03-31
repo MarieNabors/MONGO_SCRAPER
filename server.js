7d1e4960f320403766ac8466096d3dde504b385f
Marie Nabors <marienabors@thefirstacademy.org>
2018-03-31 06:44:23 -0400

    updated server.js and gitignore

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var morgan = require('morgan');
+var db = require("./models");
+var cheerio = require("cheerio");
+
 
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(morgan("dev"));
 
 app.engine('handlebars', exphbs({
 defaultLayout: 'main', extname: '.handlebars'
@@ -20,11 +24,20 @@ app.set('view engine', 'handlebars');
 
 var url ='https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=web+developer&sc.keyword=web+developer&locT=C&locId=1154247&jobType='
 require("./routes")(app);
-//port
+// Port
 var PORT = process.env.PORT || 3000;
-console.log('Marie is awesome! She got her server running on port', PORT)
 
-// LISTENER:
-// app.listen(PORT, function(){
-//   console.log('Marie is awesome! She got her server running on port', PORT)
-// });
+
+// Listener
+app.listen(PORT, function() {
+  console.log('Marie is awesome! She got her server running on port'+ PORT);
+});
+
+//Connect to Mongoose
+mongoose.connect("mongodb://localhost/jobsdb");
+var db = mongoose.connection;
+
+//Require Routes
+
+require ("./controller/routing/html-routes.js")(app);
+require ("./controller/routing/api-routes.js")(app);
